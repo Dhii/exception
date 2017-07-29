481e59aa30eec63211c9feb9da96f883b8f5ed68
Anton Ukhanev <xedin.unknown@gmail.com>
2017-07-29 21:08:32 +0200

    Added `Exception` and test

---
+++ src/Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dhii\Exception;
+
+use Exception as RootException;
+
+/**
+ * The most basic exception.
+ *
+ * @since [*next-version*]
+ */
+class Exception extends RootException implements ThrowableInterface
+{
+}
