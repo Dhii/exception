4c5cf430ba3c8b1675c2d1530e5a6b8a04383b78
Anton Ukhanev <xedin.unknown@gmail.com>
2018-02-19 11:51:13 +0100

    Added `AbstractBaseException`

---
+++ src/AbstractBaseException.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Dhii\Exception;
+
+use Exception as RootException;
+use Dhii\Util\Normalization\NormalizeStringCapableTrait;
+use Dhii\Util\Normalization\NormalizeIntCapableTrait;
+use Dhii\I18n\StringTranslatingTrait;
+
+abstract class AbstractBaseException extends RootException implements ThrowableInterface
+{
+    /* Functionality common to exceptions
+     *
+     * @since [*next-version*]
+     */
+    use ExceptionTrait;
+
+    /**
+     * Calls the parent constructor.
+     *
+     * @param string $message The error message.
+     * @param int $code The error code.
+     * @param RootException $previous The inner exception, if any.
+     *
+     * @since [*next-version*]
+     */
+    protected function _initParent($message = '', $code = 0, RootException $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * Parameter-less constructor.
+     *
+     * Invoke this in actual constructor.
+     *
+     * @since [*next-version*]
+     */
+    protected function _construct()
+    {
+    }
+}
